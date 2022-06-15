c18600fd25d95436167dcb5e7af7e6770998a5e7
Oleksii Gontar <f.gontar5@gmail.com>
2022-06-15 13:02:43 +0300

    initial setup

--- src/bin/my-git-profile.rs
+++ src/bin/my-git-profile.rs
@@ -1,64 +1,82 @@
+use seahorse::{App, Command, Context};
 use serde::{Deserialize};
-use std::{fs, env};
 use std::collections::HashMap;
 use std::fmt::Debug;
-use std::io;
-use std::process::{Command, ExitStatus};
+use std::{fs, env};
+use std::process;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 struct Profile {
   email: String,
   name: String,
-  ssh: Option<String>,
+  ssh: String,
   gpg: Option<String>,
 }
 
-fn spawn(command: &str) -> io::Result<ExitStatus> {
-  return Command::new("sh")
-    .args(&["-c", command])
-    .status();
-}
-
-fn setup_profile(profile: &Profile) {
-  println!("Profile config {:?}", profile.email);
-  spawn(&format!("git config user.name \"{}\"", profile.name)).unwrap();
-  spawn(&format!("git config user.email \"{}\"", profile.email)).unwrap();
-  match &profile.ssh {
-    Some(ssh_file) => {
-      println!("Setup ssh {}", ssh_file);
-      spawn(&format!("git config core.sshCommand \"ssh -i {} -F /dev/null\"", ssh_file)).unwrap();
-    }
-    None => {},
-  }
-  match &profile.gpg {
-    Some(gpg_key) => {
-      println!("Setup gpg signature {}", gpg_key);
-      spawn(&format!("git config user.signingkey {}", gpg_key)).unwrap();
-      spawn("git config commit.gpgsign true").unwrap();
-    }
-    None => {
-      println!("No signature provided");
-    }
-  }
+fn read_profile(target_profile: &str) -> Profile {
+    println!("Target profile {}", target_profile);
+    let home = env::var("HOME").expect("No home");
+    let contents = fs::read_to_string(format!("{}/.config/rust-tools/config.json", home))
+    .expect("Something went wrong reading the file");
+    let config: HashMap<String, Profile> = serde_json::from_str(&contents).expect("Cannot parse");
+    return config.get(target_profile).unwrap().clone();
 }
 
 fn main() {
-  println!("git config core.sshCommand \"ssh -i {} -F /dev/null\"", "test");
-  let args: Vec<String> = env::args().collect();
-  println!("args {:?}", args);
-  if args.len() != 2 {
-    println!("Wrong number of arguments");
-    return;
-  }
-  let target_profile = args.get(1).unwrap();
-  println!("Target profile {}", target_profile);
-  let home = env::var("HOME").expect("No home");
-  let contents = fs::read_to_string(format!("{}/.config/rust-tools/config.json", home))
-    .expect("Something went wrong reading the file");
-  let config: HashMap<String, Profile> = serde_json::from_str(&contents).expect("Cannot parse");
-  let profile = config.get(target_profile);
-  match profile {
-    Some(profile) => setup_profile(profile),
-    None => println!("Profile not found")
-  }
+    let args: Vec<String> = env::args().collect();
+    let app = App::new("my-git-profile")
+        .version(env!("CARGO_PKG_VERSION"))
+        .usage("my-git-profile [commands]")
+        .command(clone_command());
+    let dir_name = "git@github.com:SpotOnInc/wallet-accounts.git";
+    println!("get result: {:?}", get_dir_name(&dir_name));
+    if dir_name == "git@github.com:SpotOnInc/wallet-accounts.git" {
+        return;
+    }
+    app.run(args);
+}
+
+fn spawn(command: &str) -> bool {
+    let status = process::Command::new("sh")
+        .args(&["-c", command])
+        .status()
+        .expect("failed to execute command");
+
+    if status.success() {
+        return true
+    } else {
+        println!("Non zero exit code {}", command);
+        return false
+    }
+}
+
+fn clone_command() -> Command {
+    Command::new("clone")
+        .description("clone command")
+        .usage("my-git-profile clone [profile] [repository]")
+        .action(clone_action)
+}
+
+fn get_dir_name(repository: &str) -> Option<String> {
+    if !repository.ends_with(".git") {
+        return None;
+    }
+    let trim_repo: String = repository.chars().take(repository.len() - 4).collect();
+    trim_repo.split("/").skip(1).next();
+    Some(String::from(trim_repo.split("/").skip(1).next().expect("cannot parse")))
+}
+
+fn clone_action(c: &Context) {
+    if c.args.len() != 2 {
+        println!("Wrong args {:?}", c.args);
+        return;
+    }
+    let profile = c.args.get(0).expect("Profile flag is required");
+    let profile = read_profile(&profile);
+    println!("Profile: {:?}", profile);
+    let repository = c.args.get(1).unwrap();
+    println!("Cloning {:?}", repository);
+    if !spawn(&format!("ssh-agent bash -c \"ssh-add {}; git clone {}\"", profile.ssh, repository)) {
+        return;
+    }
 }
