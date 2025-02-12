db90ecedd10a067f8dd9dc93d23ca30b71b4c824
Oleksii Gontar <f.gontar5@gmail.com>
2025-02-12 14:43:12 +0200

    change default log level

--- src/bin/my-git-utils.rs
+++ src/bin/my-git-utils.rs
@@ -1,4 +1,5 @@
 use commands::{clone_command, setup_command};
+use env_logger::Env;
 use seahorse:: App;
 use std::env;
 
@@ -6,7 +7,7 @@ mod modules;
 mod commands;
 
 fn main() {
-  env_logger::init();
+  env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
   let args: Vec<String> = env::args().collect();
   let app = App::new("my-git-utils")
       .version(env!("CARGO_PKG_VERSION"))
